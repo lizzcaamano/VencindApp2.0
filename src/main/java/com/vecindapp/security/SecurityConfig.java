8b7581d69f80c20636c0bb7f638daa610fa401e9
lizzcaamano <lizethcaama@gmail.com>
2024-12-07 17:02:53 -0500

    Swagger funcional otra vez :)

--- src/main/java/com/vecindapp/security/SecurityConfig.java
+++ src/main/java/com/vecindapp/security/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
                                 //.requestMatchers(HttpMethod.GET,"/trabajador/all").hasRole("ADMINISTRADOR")
                                 //.requestMatchers(HttpMethod.GET,"/**").authenticated()
                                 .requestMatchers(HttpMethod.POST, "/login").permitAll()// Permite el acceso al login sin autenticación
-                                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
+                                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "vecindapi-docs/**", "/swagger-ui.html", "/swagger-resources/*", "/vecindapi-docs", "/swagger-ui-vecindapi.html", "/swagger-ui/index.html").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/**").authenticated() // Protege todas las demás rutas POST
                                 .requestMatchers(HttpMethod.GET, "/**").authenticated()  // Protege todas las rutas GET
                                 .anyRequest().permitAll()
